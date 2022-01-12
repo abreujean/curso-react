38d6220d16626234620273eab3c8ece264d2fbe1
abreujean <jeandcabreu@gmail.com>
2022-01-12 20:56:28 -0300

    Criou - componente

---
+++ src/components/HelloWord.js
@@ -0,0 +1,14 @@
+import Frase from './Frase'
+
+function HelloWord() {
+
+    return (
+        <>
+            <h1>Meu primeiro componente</h1>
+            <Frase/>
+        </>
+    )
+
+}
+
+export default HelloWord
