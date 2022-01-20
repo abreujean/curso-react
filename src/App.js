52e8493949dc1d744fd0e44090d9556f51ca0a60
abreujean <jeandcabreu@gmail.com>
2022-01-20 16:17:00 -0300

    Criação - Exemplos de props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HelloWord from './components/HelloWord';
+import SayMyName from './components/SayMyname';
 
 function App() {
   return (
     <div className="App">
       <h1 >Olá React!</h1>
       <HelloWord/>
+      <SayMyName nome="Jean"/>
     </div>
   );
 }
